dc7c794bdd4b383ad80deed5acf7d0ed039b071f
Gabriel Cesario <cesario203@outlook.com>
2020-07-07 20:49:58 -0300

    Tratando erros do servidor com cores nas mensagens ao fim de uma response

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {MatSnackBar} from "@angular/material/snack-bar";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +14,25 @@ export class ProductService {
 
   constructor(private snackBar:MatSnackBar, private httpClient: HttpClient) { }
 
-  showMessage(msg:string):void{
+  showMessage(msg:string, isError:boolean=false):void{
     this.snackBar.open(msg,'X',{
       duration:3000,
       horizontalPosition:"right",
-      verticalPosition:"top"
+      verticalPosition:"top",
+      panelClass:isError?['msg-error']:['msg-success']
     })
   }
 
   create(product:Product): Observable<Product>{
-    return this.httpClient.post<Product>(this.baseUrl, product)
+    return this.httpClient.post<Product>(this.baseUrl, product).pipe(
+      map(obj=>obj),
+      catchError(e=>this.errorHandler(e))
+    )
+  }
+
+  errorHandler(e:any):Observable<any>{
+    this.showMessage('Ocorreu um erro inesperado', true)
+    return EMPTY
   }
 
   read():Observable<Product[]>{
