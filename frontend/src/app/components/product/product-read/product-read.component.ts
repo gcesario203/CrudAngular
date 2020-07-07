208995946c2a7b47b5328d0b43f8f40dc59e8323
Gabriel Cesario <cesario203@outlook.com>
2020-07-07 20:21:25 -0300

    Aprimorando logica do componente de leitura

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -5,19 +5,23 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
-  styleUrls: ['./product-read.component.css']
+  styleUrls: ['./product-read.component.css'],
+  template:`<h1>Sem produtos cadastrado</h1>`
 })
 export class ProductReadComponent implements OnInit {
 
   products: Product[]
+  hasProd: boolean
   displayedColumns = ['id','name','price','action']
 
   constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+    let aux = 0
     this.productService.read().subscribe(products=>{
       this.products = products
-      console.log(products)
+      aux = products.length
+      aux>0?this.hasProd=true:this.hasProd=false
     })
   }
 }
